9bd8a765951f7f89603145c97c50a56735732cdc
Hayden <asianhaydenxd@gmail.com>
2022-07-16 15:50:29 -0400

    Draw points and double char width

--- voronoi.js
+++ voronoi.js
@@ -6,10 +6,6 @@ async function voronoi(width, height, points) {
     for (let y = 0; y < height; y++) {
     	for (let x = 0; x < width; x++) {
 	    const currentPoint = {x: x, y: y}
-	    if (points.includes({x, y})) {
-	        process.stdout.write("()");
-		continue;
-	    }
 	    
 	    // Algorithm for finding the closest point
 	    let nearest = points[0];
@@ -18,7 +14,14 @@ async function voronoi(width, height, points) {
 		    nearest = point;
 	        }
 	    }
-	    process.stdout.write(repChars[points.indexOf(nearest)])
+
+	    if (euclid(currentPoint, nearest) == 0) {
+		process.stdout.write("()");
+		continue;
+	    }
+
+	    process.stdout.write(repChars[points.indexOf(nearest)]);
+	    process.stdout.write(repChars[points.indexOf(nearest)]);
 	}
 	process.stdout.write("\n");
     }
@@ -26,7 +29,7 @@ async function voronoi(width, height, points) {
 
 async function generateDiagram() {
     console.log("Generating voronoi diagram...");
-    await voronoi(10, 10, [{x: 5, y: 1}, {x: 9, y: 9}]);
+    await voronoi(10, 10, [{x: 5, y: 1}, {x: 9, y: 9}, {x: 2, y: 5}]);
 }
 
 generateDiagram();
