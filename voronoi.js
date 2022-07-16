1025657bcdd8081f410e046e5bb6942765a4415d
Hayden <asianhaydenxd@gmail.com>
2022-07-16 15:46:02 -0400

    Implement plotting algorithm

--- voronoi.js
+++ voronoi.js
@@ -1,14 +1,24 @@
 const repChars = ".+;>#"
 
+let euclid = ({x: x1, y: y1}, {x: x2, y: y2}) => Math.sqrt( (x1-x2)**2 + (y1-y2)**2 )
+
 async function voronoi(width, height, points) {
     for (let y = 0; y < height; y++) {
     	for (let x = 0; x < width; x++) {
-	    if (points.contains({width, height})) {
+	    const currentPoint = {x: x, y: y}
+	    if (points.includes({x, y})) {
 	        process.stdout.write("()");
 		continue;
 	    }
 	    
 	    // Algorithm for finding the closest point
+	    let nearest = points[0];
+	    for (point of points) {
+	        if (euclid(currentPoint, point) < euclid(currentPoint, nearest)) {
+		    nearest = point;
+	        }
+	    }
+	    process.stdout.write(repChars[points.indexOf(nearest)])
 	}
 	process.stdout.write("\n");
     }
@@ -16,7 +26,7 @@ async function voronoi(width, height, points) {
 
 async function generateDiagram() {
     console.log("Generating voronoi diagram...");
-    await voronoi(10, 10, [{1, 1}, {9, 9}]);
+    await voronoi(10, 10, [{x: 5, y: 1}, {x: 9, y: 9}]);
 }
 
 generateDiagram();
